3d899c7a0242c40f6bd4168fc8d803177c0a629b
Jay Bhoi <154799187+JayBhoi965@users.noreply.github.com>
2025-07-07 01:29:31 +0530

    Update Main.java

--- Employee_Manaugement_System/src/Main.java
+++ Employee_Manaugement_System/src/Main.java
@@ -1,10 +1,15 @@
-
-
+import Employeedata.Employee;
+import Tasks_pkg.TaskCls;
 
 public class Main {
 
 	 
 	public static void main(String[] args) {
+		
+		Employee emp1 = new Employee();
+		
+		TaskCls t1 = new TaskCls();
+		t1.save(emp1);
 		
 	}
 
